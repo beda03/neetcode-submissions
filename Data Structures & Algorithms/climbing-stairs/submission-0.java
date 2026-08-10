319afda13e9b6bb52320f349707d620c2416ea45
beda03 <bediaayseki24@gmail.com>
2026-08-10 08:39:33 +0300

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n <= 2)return n;
+
+        int n1 = 1;
+        int n2 = 2;
+
+        int sol = 0;
+
+        for(int i = 3; i <= n; i++){
+            sol = n2 + n1;
+
+            n1 = n2;
+            n2 = sol;
+        }
+        return sol;
+    }
+}
